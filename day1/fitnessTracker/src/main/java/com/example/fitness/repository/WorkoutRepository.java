a04b98f22a431fd980d85b1990b37b1e789204d1
Dhamodharan <119106453+727821tucs037@users.noreply.github.com>
2023-10-05 14:36:01 +0530

    Delete day1/fitnessTracker directory

--- day1/fitnessTracker/src/main/java/com/example/fitness/repository/WorkoutRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fitness.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.fitness.model.Workout;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long> {
-}
